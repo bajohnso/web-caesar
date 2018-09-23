92a4f4eb58c0b84b21a3772234a69e7f1ae87407
Merry Prankster <blueveinedbeast@gmail.com>
2018-09-23 05:45:41 -0500

    Rendered caesar form and v1 of input processing and return.

---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<html>
+
+<head>
+    <style>
+        form {
+            background-color:#eee;
+            padding:20px;
+            margin:0 auto;
+            width:540px;
+            font:16px sans-serif;
+            border-radius:10px;
+        }
+
+        textarea { 
+            margin:10px 0;
+            width:540px;
+            height:120px;
+        }
+    </style>
+</head>
+
+<body>
+    <form method='post'>
+        <label>Rotate by
+            <input type="text" name="rot" value="0">
+        </label>
+        <textarea name="text"></textarea>
+        <input type="submit" value="Submit Query">
+    </form>
+</body>
+
+</html>
+"""
+
+    
+@app.route("/", methods=["POST"])
+def encrypt():
+    rot = request.form['rot']
+    text = request.form['text']
+    new_message = rotate_string(text,int(rot))
+    return '<h1>' + new_message + '</h1>'
+
+
+    #TO-DO: Return the encrypted string in <h1> tags.
+#    return '<h1>' + return_string + '</h1>'
+
+@app.route("/", methods=['GET'])
+def index():
+    return form
+
+app.run()
